e69b24ae0ce6fba76f79cadafde40a4373f2feea
Jitse-Jan <j.waterschoot@MI-Mac011.local>
2019-04-11 16:53:18 +0100

    Rerun Python flask article

---
+++ app.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+from flask import Flask, render_template, make_response
+from flask import redirect, request, jsonify, url_for
+
+import io
+import os
+import uuid
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.debug = True
+app._static_folder = os.path.abspath("templates/static/")
+
+@app.route('/', methods=['GET'])
+def index():
+    title = 'Create the input'
+    return render_template('layouts/index.html',
+                           title=title)
+
+@app.route('/results/<uuid>', methods=['GET'])
+def results(uuid):
+    title = 'Result'
+    data = get_file_content(uuid)
+    return render_template('layouts/results.html',
+                           title=title,
+                           data=data)
+
+@app.route('/postmethod', methods = ['POST'])
+def post_javascript_data():
+    jsdata = request.form['canvas_data']
+    unique_id = create_csv(jsdata)
+    params = { 'uuid' : unique_id }
+    return jsonify(params)
+
+@app.route('/plot/<imgdata>')
+def plot(imgdata):
+    data = [float(i) for i in imgdata.strip('[]').split(',')]
+    data = np.reshape(data, (200, 200))
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.axis('off')
+    axis.imshow(data, interpolation='nearest')
+    canvas = FigureCanvas(fig)
+    output = io.BytesIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+
+def create_csv(text):
+    unique_id = str(uuid.uuid4())
+    with open('images/'+unique_id+'.csv', 'a') as file:
+        file.write(text[1:-1]+"\n")
+    return unique_id
+
+def get_file_content(uuid):
+    with open('images/'+uuid+'.csv', 'r') as file:
+        return file.read()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ templates/static/js/script.js
@@ -0,0 +1,75 @@
+$( document ).ready(function() {
+
+   function createCanvas(parent, width, height) {
+    var canvas = document.getElementById("inputCanvas");
+    canvas.context = canvas.getContext('2d');
+    return canvas;
+  }
+
+  function init(container, width, height, fillColor) {
+    var canvas = createCanvas(container, width, height);
+    var ctx = canvas.context;
+    ctx.fillCircle = function(x, y, radius, fillColor) {
+      this.fillStyle = fillColor;
+      this.beginPath();
+      this.moveTo(x, y);
+      this.arc(x, y, radius, 0, Math.PI * 2, false);
+      this.fill();
+    };
+    ctx.clearTo = function(fillColor) {
+      ctx.fillStyle = fillColor;
+      ctx.fillRect(0, 0, width, height);
+    };
+    ctx.clearTo("#fff");
+
+    canvas.onmousemove = function(e) {
+      if (!canvas.isDrawing) {
+        return;
+      }
+      var x = e.pageX - this.offsetLeft;
+      var y = e.pageY - this.offsetTop;
+      var radius = 10;
+      var fillColor = 'rgb(102,153,255)';
+      ctx.fillCircle(x, y, radius, fillColor);
+    };
+    canvas.onmousedown = function(e) {
+      canvas.isDrawing = true;
+    };
+    canvas.onmouseup = function(e) {
+      canvas.isDrawing = false;
+    };
+  }
+
+  var container = document.getElementById('canvas');
+  init(container, 200, 200, '#ddd');
+
+  function clearCanvas() {
+    var canvas = document.getElementById("inputCanvas");
+    var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function getData() {
+    var canvas = document.getElementById("inputCanvas");
+    var imageData = canvas.context.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    var outputData = []
+    for(var i = 0; i < data.length; i += 4) {
+      var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+      outputData.push(brightness);
+    }
+    $.post( "/postmethod", {
+      canvas_data: JSON.stringify(outputData)
+    }, function(err, req, resp){
+      window.location.href = "/results/"+resp["responseJSON"]["uuid"];  
+    });
+  }
+
+  $( "#clearButton" ).click(function(){
+    clearCanvas();
+  });
+
+  $( "#sendButton" ).click(function(){
+    getData();
+  });
+});
